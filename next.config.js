a9e4203e73e7cf5701247ab60d660f38bd248ef3
AleksandrSkulinets <129200582+AleksandrSkulinets@users.noreply.github.com>
2024-12-10 17:21:16 +0200

    1
    1

--- next.config.js
+++
@@ -1,7 +0,0 @@
-// next.config.js
-module.exports = {
-  basePath: '/nextjstest',  // The subdirectory if you are deploying to a subfolder
-  assetPrefix: '/nextjstest/',  // The subdirectory for assets
-  trailingSlash: true,  // Ensure paths end with a slash
-  output: 'export',  // Tell Next.js to export as static files
-};
